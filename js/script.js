33433d2cf7d5dddaef94b663c20d11d0efcedd03
SerenaMalusa <sere.malu@gmail.com>
2024-01-18 17:29:59 +0100

    feat: add event handler for reset button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ const resetGridBtnEl = document.getElementById('header_btn');
 
 // genero la griglia da 100 celle in grid_container
 createGrid (gridContainerEl, 100);
+
+// Al click del bottone
+resetGridBtnEl.addEventListener('click', function () {
+
+    // svuoto la griglia presente
+    gridContainerEl.innerHTML = ' ';
+
+    // rigenero la griglia da 100 celle in grid_container
+    createGrid (gridContainerEl, 100);
+    
+})
