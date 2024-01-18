325eadc9bc8b50a1a72f1481699462dec2165276
SerenaMalusa <sere.malu@gmail.com>
2024-01-18 17:22:51 +0100

    feat: add function createCell

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,17 @@
 /**
  * funzione che genera la griglia
  * @param {object} container elemento html in cui stampare la griglia
- * @param {number} cellNum numero di celle
+ * @param {number} totCells numero di celle
  */
-function createGrid (container, cellNum) {
+function createGrid (container, totCells) {
     // creo variabile che sia uguale alla radice quadrata del numero di celle
-    const square = Math.sqrt(cellNum);
+    const cellsPerRow = Math.sqrt(totCells);
     
     //creo un ciclo che conta da 0 a numero di celle -1 e per ogni giro
-    for (let i=0; i < cellNum; i++) {
+    for (let i=0; i < totCells; i++) {
         
         // genero una cella (con indice giro)
-        const cell = createCell(i,square);
+        const cell = createCell(i,cellsPerRow);
 
         // la aggiungo come figlia all elem html grid_container
         container.append(cell);
@@ -20,3 +20,30 @@ function createGrid (container, cellNum) {
     
 }
 
+/**
+ * funzione che genera la cella
+ * @param {number} cellIndex indice/numero cella
+ * @param {number} cellsPerRow numero di celle per riga
+ */
+function createCell(cellIndex,cellsPerRow) {
+    
+    // creo la cella
+    const cell = document.createElement('div');
+
+    // do alla cella la classe cella
+    cell.classList.add('cell');
+
+    // do alla cella larghezza uguale alla radice del numero di celle
+    cell.style.height = `calc(100% / ${cellsPerRow})`;
+
+    // do alla cella l'attributo indice cella
+    cell.setAttribute('data-index',cellIndex+1);
+
+    // al click della cella
+    cell.addEventListener('click', function () {
+        console.log('click',cellIndex+1);
+    })
+
+    return cell;
+
+}
